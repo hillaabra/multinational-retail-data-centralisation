55db16b76e91230f99beb7b69793a70960b04f50
Hilla <hilla.abramov@gmail.com>
2023-11-05 20:17:47 +0000

    add read_db_creds method

--- multinational-retail-data-centralisation/database_utils.py
+++ multinational-retail-data-centralisation/database_utils.py
@@ -1,2 +1,15 @@
+import yaml
+
+
 class DatabaseConnector:
-  pass
+    # Method that reads the credentials in the yaml file and returns a dictionary of the credentials
+    @staticmethod
+    def read_db_creds():
+      with open('multinational-retail-data-centralisation/db_creds.yaml', 'r') as stream: # check relative filepath later
+          dict_db_creds = yaml.safe_load(stream)
+
+      return dict_db_creds
+
+test = DatabaseConnector()
+result = test.read_db_creds()
+print(result)
