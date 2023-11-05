006ad6cfe3adc7f740b504cecf1a048923673239
Hilla <hilla.abramov@gmail.com>
2023-11-05 20:51:38 +0000

    add init_db_engine method

--- multinational-retail-data-centralisation/database_utils.py
+++ multinational-retail-data-centralisation/database_utils.py
@@ -1,4 +1,5 @@
 import yaml
+from sqlalchemy import create_engine
 
 
 class DatabaseConnector:
@@ -9,6 +10,24 @@ class DatabaseConnector:
           dict_db_creds = yaml.safe_load(stream)
 
       return dict_db_creds
+
+    # Method to read the credentials from the return of read_db_creds and initialise and return an sqlalchemy database engine
+    def init_db_engine(self):
+
+      dict_db_creds = self.read_db_creds()
+
+      DATABASE_TYPE = 'postgresql'
+      DBAPI = 'psycopg2'
+      HOST = dict_db_creds['RDS_HOST']
+      USER = dict_db_creds['RDS_USER']
+      PASSWORD = dict_db_creds['RDS_PASSWORD']
+      DATABASE = dict_db_creds['RDS_DATABASE']
+      PORT = dict_db_creds['RDS_PORT']
+
+      engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+
+      return engine
+
 
 test = DatabaseConnector()
 result = test.read_db_creds()
