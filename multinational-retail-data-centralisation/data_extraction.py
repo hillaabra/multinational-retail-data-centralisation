995b0f8ec5021647f895ec7de591cf7a4fb16d40
Hilla <hilla.abramov@gmail.com>
2023-11-05 22:31:34 +0000

    add read_rds_table method

--- multinational-retail-data-centralisation/data_extraction.py
+++ multinational-retail-data-centralisation/data_extraction.py
@@ -1,8 +1,28 @@
-import boto3
-import fastapi
+
+# import boto3
+# import fastapi
 import pandas as pd
-import uvicorn
+import sqlalchemy
+# import uvicorn
+
+from database_utils import DatabaseConnector
 
 
 class DataExtractor:
-  pass
+
+  # Method to extract database table to a pandas DataFrame, which it returns
+  # Takes an instance of the DatabaseConnector class and the table name as an argument
+  @staticmethod
+  def read_rds_table(instance, table_name):
+    engine = instance.init_db_engine()
+    engine.connect()
+    pd_df = pd.read_sql_table(table_name, engine)
+    return pd_df
+
+
+# Use the read_rds_table method to extract the table containing user data and return a pandas DataFrame.
+de = DataExtractor()
+dc = DatabaseConnector()
+user_data = de.read_rds_table(dc, 'legacy_users')
+
+user_data.head()
