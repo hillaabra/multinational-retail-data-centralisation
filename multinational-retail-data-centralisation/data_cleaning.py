4f12de1d39f6d5bcc4865fc88826bc76d011f850
Hilla <hilla.abramov@gmail.com>
2023-11-06 22:23:35 +0000

    define clean_user_data method with methods so far

--- multinational-retail-data-centralisation/data_cleaning.py
+++ multinational-retail-data-centralisation/data_cleaning.py
@@ -1,17 +1,50 @@
 # %%
-import re
+# import re # needed?
 import numpy as np
 import pandas as pd
 from  data_extraction import user_data
 
-print(type(user_data))
-# I need to figure ou
+
 class DataCleaning:
 
     # Method to clean the user data (look for NULL values,
     # errors with dates, incorrectly typed values and rows filled with the wrong info)
     def clean_user_data():
-        pass
+
+        ud_df = user_data.copy()
+
+        # Based on tests done on copied dataframe, I want to:
+
+        # Set index column
+        ud_df.set_index('index', inplace=True)
+
+        # Cast the "first_name" and "last_name" values to strings
+        string_value_columns = ['first_name', 'last_name', 'company', 'email_address', 'address', 'country', 'user_uuid']
+
+        for column in string_value_columns:
+            ud_df[column] = ud_df[column].astype('string')
+
+        # Delete all rows where the "first_name" is NULL using dropna
+        # ud_df.dropna(subset='first_name', inplace=True)
+        # Is this still needed?
+
+        # Delete all rows where the "first_name" is equal to "NULL" string
+        mask_null_string_for_first_name = ud_df[ud_df['first_name'] == "NULL"]
+        ud_df = ud_df[~mask_null_string_for_first_name]
+
+        # Delete all rows where the "first_name" value contains a numeric digit
+        mask_numeric_digit_in_first_name = ud_df['first_name'].str.contains(pat='[0-9]', regex=True)
+        ud_df = ud_df[~mask_numeric_digit_in_first_name]
+
+        # Convert the "date_of_birth" column to datetime values
+        # (either using dateutil.parse parser, or pd.to_datetime with argument "mixed"
+        date_value_columns = ['date_of_birth', 'join_date']
+
+        for column in date_value_columns:
+            ud_df[column] = pd.to_datetime(ud_df[column], format='mixed', errors='coerce') # or use dateutil.parse parser?
+
+
+        # convert country and country code into category datatypes (and make uniform) - also delete one of these columns - redundant?
 
 # %%
 user_data.head()
@@ -44,3 +77,40 @@ ud_copy.date_of_birth.info()
 # %%
 ud_copy.date_of_birth.describe()
 # COME BACK TO THIS - the max value is 20th Nov 2006 - is this invalid? 17 years old....
+
+# %%
+ud_copy.loc[ud_copy["last_name"].str.match(".*[0-9]+.*", "last_name")]
+# %%
+ud_copy.loc[ud_copy['first_name'].str.contains(pat='[0-9]', regex=True)]
+# %%
+ud_copy.head()
+# %%
+ud_copy.sort_values(by='date_of_birth', axis=0).tail(50)
+
+# %%
+# Since all rows with a null first_name also has a null last_name, I'm deleting all rows with a null first_name
+ud_copy.dropna(subset='first_name', inplace=True)
+# THIS ISN't WORKING BECAUSE THERE ARE NULL STRINGS
+
+# %%
+ud_copy.sort_values(by='date_of_birth', axis=0).tail(50)
+# %%
+mask_null_strings = ud_copy['first_name'] == "NULL"
+# %%
+ud_copy[~mask_null_strings].sort_values(by='date_of_birth', axis=0).tail(50)
+# %%
+mask_null_strings_orig = user_data['first_name'] == "NULL"
+user_data[mask_null_strings_orig]
+# %%
+## TRY THIS LATER
+for col in user_data:
+    user_data[col].str.contains(pat="NULL")
+# %%
+
+# %%
+ud_copy.join_date = pd.to_datetime(ud_copy.join_date, format='mixed', errors='coerce')
+# %%
+ud_copy.sort_values(by='join_date', axis=0).head(50)
+# %%
+ud_copy.join_date.describe()
+# %%
