6537af538a3784181eee1fc36add39bd505be12b
Hilla <hilla.abramov@gmail.com>
2023-11-07 18:59:07 +0000

    add upload_to_db method

--- multinational-retail-data-centralisation/database_utils.py
+++ multinational-retail-data-centralisation/database_utils.py
@@ -3,7 +3,6 @@ import psycopg2
 import yaml
 from sqlalchemy import create_engine, inspect
 
-
 class DatabaseConnector:
     # Method that reads the credentials in the yaml file and returns a dictionary of the credentials
     @staticmethod
@@ -36,12 +35,18 @@ class DatabaseConnector:
       table_names = inspector.get_table_names()
       return table_names
 
-test = DatabaseConnector()
+    # method that takes in a Pandas DataFrame and table name to upload to as an argument
+    def upload_to_db(self, pd_df, table_name):
+      engine = self.init_db_engine()
+      pd_df.to_sql(table_name, engine, if_exists='replace')
+
+
 # engine = test.init_db_engine()
 # inspector = inspect(engine)
 # result = inspector.get_table_names()
 # print(result)
 
 if __name__ == "__main__":
+  test = DatabaseConnector()
   result = test.list_db_tables()
   print(result)
