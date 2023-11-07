6ec4f434d487f4b895e73db1efd92846061228a8
Hilla <hilla.abramov@gmail.com>
2023-11-07 20:44:21 +0000

    add main.py file

---
+++ multinational-retail-data-centralisation/main.py
@@ -0,0 +1,10 @@
+
+# %%
+from data_cleaning import DataCleaning
+from database_utils import DatabaseConnector
+
+# %%
+dc = DatabaseConnector()
+cleaned_user_data = DataCleaning.clean_user_data()
+# %%
+dc.upload_to_db(cleaned_user_data, 'dim_users')
