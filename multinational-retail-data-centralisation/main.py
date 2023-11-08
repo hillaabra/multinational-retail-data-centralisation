f99cbfe2df0de9bd6d12aabf1d7cebcaeb673741
Hilla <hilla.abramov@gmail.com>
2023-11-08 19:38:24 +0000

    upload cleaned card_data to database

--- multinational-retail-data-centralisation/main.py
+++ multinational-retail-data-centralisation/main.py
@@ -1,11 +1,11 @@
-
-# %%
 from data_cleaning import DataCleaning
 from database_utils import DatabaseConnector
 
-# %%
-dc = DatabaseConnector()
-cleaned_user_data = DataCleaning.clean_user_data()
-# %%
-dc.upload_to_db(cleaned_user_data, 'dim_users')
-# %%
+data_cleaner = DataCleaning()
+cleaned_user_data = data_cleaner.clean_user_data()
+
+conn = DatabaseConnector()
+conn.upload_to_db(cleaned_user_data, 'dim_users')
+
+cleaned_card_data = data_cleaner.clean_card_data()
+conn.upload_to_db(cleaned_card_data, 'dim_card_details')
