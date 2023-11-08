759b88a3ccb05d98ba858ec07f995b46ec54fcb8
Hilla <hilla.abramov@gmail.com>
2023-11-08 23:14:50 +0000

    create list_number_of_stores and retrieve_stores_data methods

--- multinational-retail-data-centralisation/data_extraction.py
+++ multinational-retail-data-centralisation/data_extraction.py
@@ -1,16 +1,10 @@
-
-# import boto3
-# import fastapi
-# %%
+import json
 import pandas as pd
+import requests
 import sqlalchemy
-# %%
 import tabula
-# import uvicorn
-
 from database_utils import DatabaseConnector
 
-# %%
 class DataExtractor:
 
   # Method to extract database table to a pandas DataFrame, which it returns
@@ -32,12 +26,73 @@ class DataExtractor:
 
     return dfs
 
-# %%
-# Use the read_rds_table method to extract the table containing user data and return a pandas DataFrame.
+  @staticmethod
+  def retrieve_api_authorisation():
+    with open('.secret/config.json', 'r') as read_file:
+      header_dict = json.load(read_file)
+
+    return header_dict
+
+  # method to return the number of stores to extract from API
+  def list_number_of_stores(self, endpoint, header_dict):
+
+    response = requests.get(endpoint, headers=header_dict)
+
+    if response.status_code == 200:
+
+      return response
+
+    else:
+
+      print(response.status_code)
+
+  # method to retrieve store data from API and return it in a dataframe
+  def retrieve_stores_data(self, endpoint):
+
+    header_dict = self.retrieve_api_authorisation()
+
+    num_of_stores_response = self.list_number_of_stores("https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores", header_dict)
+    num_of_stores = num_of_stores_response.json()['number_stores']
+
+    response = requests.get(f"{endpoint}0", headers=header_dict)
+
+    if response.status_code == 200:
+
+      store_data = response.json()
+      df_store_data = pd.DataFrame([store_data])
+      df_store_data.set_index('index', inplace=True)
+
+    else:
+
+      print(response.status_code)
+
+    for i in range(1, num_of_stores):
+
+      response = requests.get(f"{endpoint}{i}", headers=header_dict)
+
+      if response.status_code == 200:
+
+        store_data = response.json()
+        df_new_store_data = pd.DataFrame([store_data])
+        df_store_data = pd.concat([df_store_data, df_new_store_data], ignore_index=True)
+
+      else:
+
+        print(response.status_code)
+
+    return df_store_data
+
+
+
 de = DataExtractor()
 dc = DatabaseConnector()
+
+# Use the read_rds_table method to extract the table containing user data and return a pandas DataFrame
 user_data = de.read_rds_table(dc, 'legacy_users')
 
+# Use the retrieve_pdf_data method to extract the table containing card_data from the pdf link
 pdf_link = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"
 card_data = de.retrieve_pdf_data(pdf_link)
 
+# Use the retrieve_stores_data method to extract the store data from the API and return it as a dataframe
+store_data = de.retrieve_stores_data("https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/")
