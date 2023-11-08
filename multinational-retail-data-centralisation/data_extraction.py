75c76f9873e32c05ef00876f42e5cc42c1341922
Hilla <hilla.abramov@gmail.com>
2023-11-08 00:25:03 +0000

    import tabula, define retrieve pdf method, retrieve card_data

--- multinational-retail-data-centralisation/data_extraction.py
+++ multinational-retail-data-centralisation/data_extraction.py
@@ -1,13 +1,16 @@
 
 # import boto3
 # import fastapi
+# %%
 import pandas as pd
 import sqlalchemy
+# %%
+import tabula
 # import uvicorn
 
 from database_utils import DatabaseConnector
 
-
+# %%
 class DataExtractor:
 
   # Method to extract database table to a pandas DataFrame, which it returns
@@ -17,11 +20,24 @@ class DataExtractor:
     engine = instance.init_db_engine()
     engine.connect()
     pd_df = pd.read_sql_table(table_name, engine)
+    engine.dispose()
     return pd_df
 
+  # method to extract data from a pdf document. takes link as argument. extracts all pages.
+  # returns dataframe of extracted data
+  @staticmethod
+  def retrieve_pdf_data(link):
 
+    dfs = tabula.read_pdf(link, lattice=True, pages='all', multiple_tables=False)[0]
+
+    return dfs
+
+# %%
 # Use the read_rds_table method to extract the table containing user data and return a pandas DataFrame.
-
 de = DataExtractor()
 dc = DatabaseConnector()
 user_data = de.read_rds_table(dc, 'legacy_users')
+
+pdf_link = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"
+card_data = de.retrieve_pdf_data(pdf_link)
+
