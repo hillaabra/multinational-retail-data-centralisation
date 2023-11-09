922a578383ad2c105cecf0d2ef89da5fda697f9e
Hilla <hilla.abramov@gmail.com>
2023-11-09 03:19:24 +0000

    cleaned stores data and uploaded it to database as dim_store_details table

--- multinational-retail-data-centralisation/main.py
+++ multinational-retail-data-centralisation/main.py
@@ -1,11 +1,17 @@
+# %%
 from data_cleaning import DataCleaning
 from database_utils import DatabaseConnector
-
+# %%
 data_cleaner = DataCleaning()
-cleaned_user_data = data_cleaner.clean_user_data()
-
 conn = DatabaseConnector()
+# %%
+cleaned_user_data = data_cleaner.clean_user_data()
 conn.upload_to_db(cleaned_user_data, 'dim_users')
-
+# %%
 cleaned_card_data = data_cleaner.clean_card_data()
 conn.upload_to_db(cleaned_card_data, 'dim_card_details')
+
+# %%
+cleaned_stores_data = data_cleaner.clean_stores_data()
+conn.upload_to_db(cleaned_stores_data, 'dim_store_details')
+# %%
