0d19cfdb4d4d869fd64d60b0732d31cd8ffbc5e8
Hilla <hilla.abramov@gmail.com>
2023-11-14 16:26:08 +0000

    add set_varchar_integer_to_max_length_of_column and helper methods

--- multinational-retail-data-centralisation/database_utils.py
+++ multinational-retail-data-centralisation/database_utils.py
@@ -1,6 +1,6 @@
 import psycopg2
 import yaml
-from sqlalchemy import create_engine, inspect
+from sqlalchemy import create_engine, inspect, text
 
 class DatabaseConnector:
     # Method that reads the credentials in the yaml file and returns a dictionary of the credentials
@@ -52,11 +52,28 @@ class DatabaseConnector:
       return engine
 
     # method that takes in a Pandas DataFrame and table name to upload to as an argument
-    def upload_to_local_db(self, pd_df, table_name):
+    def upload_to_local_db(self, pd_df, table_name, dtypes):
       engine = self.init_local_db_engine()
       engine.execution_options(isolation_level='AUTOCOMMIT').connect()
-      pd_df.to_sql(table_name, engine, if_exists='replace')
+      pd_df.to_sql(table_name, engine, if_exists='replace', dtype=dtypes)
       engine.dispose()
 
+    # method that takes in a query_text
+    def update_local_db(self, query_text: str):
+       engine = self.init_local_db_engine()
+       with engine.execution_options(isolation_level='AUTOCOMMIT').connect() as conn:
+          conn.execute(text(query_text))
 
+    @staticmethod
+    def get_max_length_of_table_column(engine, table_name: str, column_name: str):
+        with engine.execution_options(isolation_level='AUTOCOMMIT').connect() as conn:
+            result = conn.execute(text(f"SELECT MAX(LENGTH({column_name})) FROM {table_name};")).fetchone()
+            # this is returned as a tuple (e.g. (12, 0))
+        return result[0] # index to get just the numeric value
 
+    def set_varchar_integer_to_max_length_of_column(self, table_name: str, column_name: str):
+        engine = self.init_local_db_engine()
+        max_length = self.get_max_length_of_table_column(engine, table_name, column_name)
+        query = f"ALTER TABLE {table_name} ALTER COLUMN {column_name} TYPE VARCHAR({max_length});"
+        with engine.execution_options(isolation_level='AUTOCOMMIT').connect() as conn:
+            result = conn.execute(text(query))
