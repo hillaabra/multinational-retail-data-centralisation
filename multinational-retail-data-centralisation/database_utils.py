7b8d16cc51940761cbe127753a80ea10aee4b3f3
Hilla <hilla.abramov@gmail.com>
2023-11-15 16:39:41 +0000

    create subclasses for local and remote database mmethods

--- multinational-retail-data-centralisation/database_utils.py
+++ multinational-retail-data-centralisation/database_utils.py
@@ -2,19 +2,37 @@ import psycopg2
 import yaml
 from sqlalchemy import create_engine, inspect, text
 
-class DatabaseConnector:
+# I could also have the different dataset classes inherit from the localdatabase class so that the
+# data schema methods can be called directly from inside the class? table_name attributes assigned with initialisation?
+
+from abc import ABC, abstractmethod
+
+class DatabaseConnector(ABC):
+
     # Method that reads the credentials in the yaml file and returns a dictionary of the credentials
-    @staticmethod
-    def read_db_creds(yaml_file):
-      with open(yaml_file, 'r') as stream: # check relative filepath later
+    def __init__(self, credentials_yaml):
+       self.credentials_yaml = credentials_yaml
+
+    def _read_db_creds(self):
+      with open(self.credentials_yaml, 'r') as stream: # check relative filepath later
           dict_db_creds = yaml.safe_load(stream)
 
       return dict_db_creds
 
-    # Method to read the credentials from the return of read_db_creds and initialise and return an sqlalchemy database engine
-    def init_remote_db_engine(self):
+    @abstractmethod
+    def _init_db_engine(self):
+       pass
 
-      dict_db_creds = self.read_db_creds('.credentials/remote_db_creds.yaml')
+
+class RDSDatabaseConnector(DatabaseConnector):
+
+    def __init__(self):
+       super().__init__('.credentials/remote_db_creds.yaml')
+
+    # Method to read the credentials from the return of read_db_creds and initialise and return an sqlalchemy database engine
+    def _init_db_engine(self):
+
+      dict_db_creds = self._read_db_creds()
 
       conf = {
         'HOST': dict_db_creds['RDS_HOST'],
@@ -29,16 +47,21 @@ class DatabaseConnector:
 
     # method to list all the tables in the database so you know which tables you can extract data from
     def list_db_tables(self):
-      engine = self.init_remote_db_engine()
+      engine = self._init_db_engine()
       inspector = inspect(engine)
       table_names = inspector.get_table_names()
       return table_names
 
 
-    # method to connect to local pgadmin database
-    def init_local_db_engine(self):
+class LocalDatabaseConnector(DatabaseConnector):
 
-      dict_db_creds = self.read_db_creds('.credentials/local_db_creds.yaml')
+    def __init__(self):
+       super().__init__('.credentials/local_db_creds.yaml')
+
+    # method to connect to local pgadmin database
+    def _init_db_engine(self):
+
+      dict_db_creds = self._read_db_creds()
 
       DATABASE_TYPE = dict_db_creds['DATABASE_TYPE']
       DBAPI = dict_db_creds['DBAPI']
@@ -53,28 +76,36 @@ class DatabaseConnector:
       return engine
 
     # method that takes in a Pandas DataFrame and table name to upload to as an argument
-    def upload_to_local_db(self, pd_df, table_name, dtypes=None):
-      engine = self.init_local_db_engine()
+    def upload_to_db(self, pd_df, table_name, dtypes=None):
+      engine = self._init_db_engine()
       engine.execution_options(isolation_level='AUTOCOMMIT').connect()
       pd_df.to_sql(table_name, engine, if_exists='replace', dtype=dtypes) # alter method so that I can call it with a dictionary too
       engine.dispose()
 
     # method that takes in a query_text
-    def update_local_db(self, query_text: str):
-       engine = self.init_local_db_engine()
+    def update_db(self, query_text: str):
+       engine = self._init_db_engine()
        with engine.execution_options(isolation_level='AUTOCOMMIT').connect() as conn:
           conn.execute(text(query_text))
 
     @staticmethod
-    def get_max_length_of_table_column(engine, table_name: str, column_name: str):
+    def _get_max_length_of_table_column(engine, table_name: str, column_name: str):
         with engine.execution_options(isolation_level='AUTOCOMMIT').connect() as conn:
-            result = conn.execute(text(f"SELECT MAX(LENGTH({column_name})) FROM {table_name};")).fetchone()
-            # this is returned as a tuple (e.g. (12, 0))
+            query = f"SELECT MAX(LENGTH({column_name})) FROM {table_name};"
+            result = conn.execute(text(query)).fetchone() # this is returned as a tuple (e.g. (12, 0))
         return result[0] # index to get just the numeric value
 
     def set_varchar_integer_to_max_length_of_column(self, table_name: str, column_name: str):
-        engine = self.init_local_db_engine()
-        max_length = self.get_max_length_of_table_column(engine, table_name, column_name)
-        query = f"ALTER TABLE {table_name} ALTER COLUMN {column_name} TYPE VARCHAR({max_length});"
+        engine = self._init_db_engine()
+        max_length = self._get_max_length_of_table_column(engine, table_name, column_name)
+        query = f'ALTER TABLE {table_name} ALTER COLUMN "{column_name}" TYPE VARCHAR({max_length});'
         with engine.execution_options(isolation_level='AUTOCOMMIT').connect() as conn:
             conn.execute(text(query))
+
+    def print_data_types_of_columns_in_specified_table(self, table_name: str):
+        engine = self._init_db_engine()
+        query = f"SELECT column_name, data_type, character_maximum_length, numeric_precision, numeric_precision_radix, datetime_precision, udt_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{table_name}';"
+        with engine.execution_options(isolation_level='AUTOCOMMIT').connect() as conn:
+            result = conn.execute(text(query))
+            for row in result:
+               print(row) # currently this is printing without the column headings - try with psycopg maybe?
