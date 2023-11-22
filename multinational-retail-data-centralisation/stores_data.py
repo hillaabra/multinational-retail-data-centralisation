ade6f59e2c3205a2bcb1c8e1683ff3b051279452
Hilla <hilla.abramov@gmail.com>
2023-11-22 12:16:33 +0000

    define extract_data method from abc and define retrieve_stores_data methods within the child class

--- multinational-retail-data-centralisation/stores_data.py
+++ multinational-retail-data-centralisation/stores_data.py
@@ -1,3 +1,6 @@
+import requests
+import pandas as pd
+
 from sqlalchemy.dialects.postgresql import DATE, VARCHAR
 
 from data_extraction import DataExtractor
@@ -9,11 +12,87 @@ class StoresData(DataExtractor, DataCleaning, DatabaseTableConnector):
 
     def __init__(self):
         try:
-          DataExtractor.__init__(self, source_type='api_resource', source_location='https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/')
+          DataExtractor.__init__(self, source_data_url='https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/')
           DatabaseTableConnector.__init__(self, table_name='dim_store_details')
         except Exception:
           print("Something went wrong initialising the StoresData child class")
 
+    # method to return the number of stores to extract from API
+    @staticmethod
+    def _get_number_of_stores(endpoint, header_dict) -> int:
+
+      response = requests.get(endpoint, headers=header_dict)
+
+      if response.status_code == 200:
+        num_of_stores = response.json()['number_stores']
+        return num_of_stores
+
+      else:
+
+        print(response.status_code)
+
+    # method to get extract first row of store data from API and return it in a dataframe
+    def _initialise_stores_df_loading(self, endpoint, header_dict) -> pd.DataFrame:
+
+        response = requests.get(f"{endpoint}0", headers=header_dict)
+
+        if response.status_code == 200:
+
+            store_data = response.json()
+            df_store_data = pd.DataFrame([store_data])
+            df_store_data.set_index('index', inplace=True)
+
+            return df_store_data
+
+        else:
+
+            print(response.status_code)
+             # return something here?
+
+    # method to extract the remaining rows of store data and add them to the created dataframe
+    def _add_remaining_stores_as_rows_to_df(self, df_store_data, num_of_stores, endpoint, header_dict) -> pd.DataFrame:
+
+        for i in range(1, num_of_stores):
+
+          response = requests.get(f"{endpoint}{i}", headers=header_dict)
+
+          if response.status_code == 200:
+
+            store_data = response.json()
+            df_new_store_data = pd.DataFrame([store_data])
+            df_store_data = pd.concat([df_store_data, df_new_store_data], ignore_index=True)
+
+          else:
+
+            print(response.status_code)
+
+        return df_store_data
+
+    # method to retrieve the stores data from the API endpoint
+    def _retrieve_stores_data(self) -> pd.DataFrame:
+
+        header_dict = self.__retrieve_api_authorisation()
+
+        endpoint = self._source_data_url
+
+        df_store_data = self._initialise_stores_df_loading(endpoint, header_dict)
+
+        num_of_stores = self._get_number_of_stores("https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores", header_dict)
+
+        if num_of_stores is not None:
+            df_store_data = self._add_remaining_stores_as_rows_to_df(df_store_data, num_of_stores, endpoint, header_dict)
+
+            return df_store_data
+        else:
+           print("Error: Could not retrieve num_of_stores from API.")
+
+    # defining abstract method from DataExtractor base class
+    # this method assigns the dataframe of extracted data to the extracted_data attribute
+    def extract_data(self) -> None:
+       extracted_data_df = self._retrieve_stores_data()
+       self._extracted_data = extracted_data_df
+
+    # defining abstract method from DataCleaning abstract base class
     def clean_extracted_data(self) -> None:
 
         sd_df = self._extracted_data.copy()
