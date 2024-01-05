2ed17f25b2b1e747afb2823274cc4a6910de619a
Hilla <hilla.abramov@gmail.com>
2024-01-05 17:11:35 +0000

    fix: filepath to api credentials in stores_data_config

--- db_setup/datasets/config.py
+++ db_setup/datasets/config.py
@@ -13,7 +13,7 @@ products_data_config = {"target_table_name": "dim_products",
 stores_data_config = {"target_table_name": "dim_store_details",
                       "store_details_endpoint": "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/",
                       "num_of_stores_endpoint": "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores",
-                      "api_credentials_filepath": ".credentials/api_config.json"} # check this filepath is compatible from where it gets called
+                      "api_credentials_filepath": "db_setup/.credentials/api_config.json"}
 
 user_data_config = {"target_table_name": "dim_users",
                     "source_db_table_name": "legacy_users"}
